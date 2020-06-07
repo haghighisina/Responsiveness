8def0618a48619286f1b4612571f94e93065fa55
haghighisina <khabbazhaghighi@gmail.com>
2020-06-07 16:18:40 +0430

    responsiveness

---
+++ index.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="Style.css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,500&display=swap" rel="stylesheet">
+    <title>Responsive web design</title>
+</head>
+<body>
+<header>
+    <div class="logo-container">
+        <img src="logo.png" alt="logo">
+        <h4 class="logo">Hello there</h4>
+    </div>
+    <nav>
+        <ul class="nav-links">
+            <li><a class="nav-link" href="#">Specs</a></li>
+            <li><a class="nav-link" href="#">Product</a></li>
+            <li><a class="nav-link" href="#">Contact</a></li>
+        </ul>
+    </nav>
+    <div class="cart">
+        <img src="cart.png" alt="cart">
+    </div>
+</header>
+
+<main>
+    <section class="presentation">
+        <div class="introduction">
+            <div class="intro-tex">
+                <h1>Hello This Is A Website </h1>
+                <p>This is the end khkj bkjbh kjbh </p>
+            </div>
+            <div class="cta">
+                <button class="cta-select">New</button>
+                <button class="cta-add">Add</button>
+            </div>
+        </div>
+        <div class="cover">
+            <img src="3.jpg" alt="book">
+        </div>
+
+        <img class="first" src="1.png" alt="">
+        <img class="middle" src="1.png" alt="">
+        <img class="bottom" src="1.png" alt="">
+    </section>
+</main>
+</body>
+</html>
